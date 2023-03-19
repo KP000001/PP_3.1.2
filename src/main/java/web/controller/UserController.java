395817d2d15154736ae7a96116a84af132178318
Pavel Kudriakov <kudryakov.p@yandex.ru>
2023-03-20 01:50:57 +0400

    ver.01_20.03.23

--- src/main/java/web/controller/UserController.java
+++ src/main/java/web/controller/UserController.java
@@ -2,9 +2,8 @@ package web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.*;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
 import web.model.User;
 import web.service.UserService;
 
